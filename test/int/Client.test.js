7ef94648b806c3ff0da41cfffcb6d931cdd11014
Christian Froehlingsdorf <christian.froehlingsdorf@rewe-digital.com>
2017-12-20 15:51:28 +0100

    now throwing response body error message as part of the error on bad requests

--- test/int/Client.test.js
+++ test/int/Client.test.js
@@ -72,7 +72,7 @@ describe("Client INT", () => {
             await client.raw("/badcode", {}, 200);
         } catch(error){
             errorThrown = true;
-            assert.equal(error.message, "Response status code: 400 does match expected status code: 200.");
+            assert.equal(error.message, "Response status code: 400 does match expected status code: 200. No error message present in body.");
         }
     
         assert.ok(errorThrown);
